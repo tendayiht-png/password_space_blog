82e56c84a92e41fbffb6166b19b8f39d653ffb24
tendayiht-png <tendayiht@gmail.com>
2026-03-07 14:24:40 +0000

    enable serving of static files

--- settings.py
+++ settings.py
@@ -21,11 +21,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = "django-insecure-oz+!92^#ojc95rt2x$b07^l!gp4hg6(qmd$!a3*2jyrezfm*nlMake up your own randomSecretKey"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
