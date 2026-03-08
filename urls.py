08773f2dba0f460e7f8bdee2829e26c48e858ac3
tendayiht-png <tendayiht@gmail.com>
2026-03-08 18:00:59 +0000

    Integrate wireframe templates and create dedicated pages

--- urls.py
+++ urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('how-to-2fa/', views.HowToPageView.as_view(), name='how_to_page'),
+    path('breach-checklist/', views.ChecklistPageView.as_view(), name='checklist_page'),
 ]
